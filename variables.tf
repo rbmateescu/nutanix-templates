9d5333270d8e6904d28a2191fa0c5d6a46e8d1ee
mateescu <mateescu@ca.ibm.com>
2018-07-19 12:01:21 -0400

    Nutanix template

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "username" {
+  description = "Nutanix username."
+}
+variable "password" {
+  description = "Nutanix password."
+}
+variable "endpoint" {
+  description = "Nutanix endpoint."
+}
+variable "cluster" {
+  description = "Nutanix password"
+}
